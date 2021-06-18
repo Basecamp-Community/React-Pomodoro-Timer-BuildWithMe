1b33c45c808f69f5b78dd60cd14749bd283ea8d2
Rishabh Singh <rishabhsinghs90s@gmail.com>
2021-06-18 15:57:28 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import "./App.css";
 
-function App() {
+const padTime = (time) => {
+  return time.toString().padStart(2, "0");
+};
+
+const App = () => {
+  // State Variables
+  const [timeLeft, setTimeLeft] = useState(25 * 60);
+  const [title, setTitle] = useState("Let the countdown begin!");
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
+
+  // Functions
+  const startTimer = () => {
+    if (intervalRef.current !== null) return;
+
+    setTitle("You are doing great!");
+    setIsRunning(true);
+
+    intervalRef.current = setInterval(() => {
+      setTimeLeft((timeLeft) => {
+        if (timeLeft > 0) return timeLeft - 1;
+
+        resetTimer()
+
+        return 0;
+      });
+    }, 1000);
+  };
+
+  const stopTimer = () => {
+    if (intervalRef.current === null) return;
+
+    setTitle("Keep it going!");
+    setIsRunning(false);
+
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+  };
+
+  const resetTimer = () => {
+    setTitle("Ready for another round!");
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTimeLeft(25 * 60);
+    setIsRunning(false);
+  };
+
+  // Computing
+  const minutes = padTime(Math.floor(timeLeft / 60));
+  const seconds = padTime(timeLeft - minutes * 60);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>{title}</h1>
+
+      <div className="timer">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+
+      <div className="buttons">
+        {!isRunning && <button onClick={startTimer}>Start</button>}
+        {isRunning && <button onClick={stopTimer}>Stop</button>}
+        <button onClick={resetTimer}>Reset</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
